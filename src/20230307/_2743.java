dca02f02e698f04e0516f4e49ee8cdd82bf88f77
minseok128 <minseok128128@gmail.com>
2023-03-07 22:41:14 +0900

    Solve 2743

--- src/20230307/_2743.java
+++ src/20230307/_2743.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class _2743 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int len = sc.nextLine().length();
-		System.out.println(len);
+		int N = sc.nextInt();
+		String str;
+
+		for (int i = 0; i < N; i++) {
+			str = sc.next();
+			System.out.print(str.charAt(0));
+			System.out.println(str.charAt(str.length() - 1));
+		}
 		sc.close();
 	}
 }
