d07a976d1095ef82fb08f78e6ac0389b98e09e89
minseok128 <minseok128128@gmail.com>
2023-03-07 22:28:24 +0900

    Solve 2743

---
+++ src/20230307/_2743.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class _2743 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int len = sc.nextLine().length();
+		System.out.println(len);
+		sc.close();
+	}
+}
