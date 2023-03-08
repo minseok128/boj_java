ed4eb1acebea68df661d28b948263029208d93ef
minseok128 <minseok128128@gmail.com>
2023-03-08 18:51:32 +0900

    Solve 11718

---
+++ src/20230308/_11718.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class _11718 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str;
+		while (sc.hasNextLine()) {
+			str = sc.nextLine();
+			System.out.println(str);
+		}
+		sc.close();
+		return;
+	}
+}
