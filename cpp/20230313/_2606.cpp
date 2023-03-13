5522f46ffbc7822fa8be10c2d48b0b51cf606170
minseok128 <minseok128128@gmail.com>
2023-03-13 10:39:08 +0900

    Initialize cpp

---
+++ cpp/20230313/_2606.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+
+	cin >> n;
+	
+	
+	return (0);
+}
